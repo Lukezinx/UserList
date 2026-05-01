bb0840f3920be1207c623a3847a5bde6086b38f8
Lucas <lucasdeapool@gmail.com>
2026-05-01 18:40:24 -0300

    create a list of users

---
+++ src/components/AlbumsList.js
@@ -0,0 +1,7 @@
+function AlbumsList({user}) {
+    return <div>
+        albums for {user.name}
+    </div>
+}
+
+export default AlbumsList
---
+++ src/components/UsersListItem.js
@@ -0,0 +1,34 @@
+import { GoTrashcan } from "react-icons/go"
+import Button from "./Button"
+import { removeUser } from "../store"
+import { useThunk } from "../hooks/use-thunk"
+import ExpandablePanel from "./ExpandablePanel"
+import AlbumsList from "./AlbumsList"
+
+
+function UserListItem({user}) {
+
+    const [doRemoverUser, isLoading, error] = useThunk(removeUser)
+
+    const handleClick = () => {
+        doRemoverUser(user)
+    }
+
+    const header = <>
+        <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+            <GoTrashcan/>
+        </Button>
+        {error && <div>Error deleting user. </div>}
+        {user.name}
+        </>
+
+    return(
+        <ExpandablePanel header={header}>
+            <AlbumsList user={user}/>
+        </ExpandablePanel>
+    );
+                       
+                    
+}
+
+export default UserListItem
